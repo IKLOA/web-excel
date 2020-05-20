063ce36cd158418015d59940f92fc9f693b5870b
IKLOA <fandor1q@yandex.com>
2020-05-21 01:58:36 +0300

    finish base table logic

--- src/core/utils.js
+++ src/core/utils.js
@@ -4,3 +4,14 @@ export function capitalize(string) {
   }
   return string.charAt(0).toUpperCase() + string.slice(1)
 }
+
+export function range(start, end) {
+  if (start > end) {
+    const swap = start
+    start = end
+    end = swap
+  }
+  return new Array(end - start + 1).fill('').map((_, index) => {
+    return start + index
+  })
+}
